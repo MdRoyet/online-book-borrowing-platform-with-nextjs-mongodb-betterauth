9a5f3fc7161d6a4f627689d18b786ebbe1d896f0
Md Royet <rroyet8@gmail.com>
2026-05-01 21:48:20 +0600

    folder creation for components

---
+++ src/components/books/CategorySidebar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const CategorySidebar = () => {
+  return (
+    <div>
+      <h2>CategorySidebar</h2>
+    </div>
+  );
+};
+
+export default CategorySidebar;
---
+++ src/components/layout/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const NavBar = () => {
+  return (
+    <div>
+      <h2>NavBar will be there</h2>
+    </div>
+  );
+};
+
+export default NavBar;
